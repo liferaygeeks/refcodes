9a4080c701b27f2cd18546c43f5d4b9f68472878
AbdulRazakh <abdulrazakh.brb@gmail.com>
2017-03-21 23:19:44 +0300

    Check

---
+++ hooks/BizenRegistration/src/main/java/com/bizen/project/SiteCreationAction.java
@@ -0,0 +1,18 @@
+package com.bizen.project;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+import com.liferay.portal.kernel.struts.StrutsAction;
+
+public class SiteCreationAction extends BaseStrutsAction {
+
+	@Override
+	public String execute(StrutsAction originalStrutsAction,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute(originalStrutsAction, request, response);
+	}
+}
