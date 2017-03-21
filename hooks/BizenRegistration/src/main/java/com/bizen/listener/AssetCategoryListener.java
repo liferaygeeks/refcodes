d72c9036303c832b64f4c954fd308b87b6956bb3
AbdulRazakh <abdulrazakh.brb@gmail.com>
2017-03-21 22:58:17 +0300

    Creating Site using API addGroup

--- hooks/BizenRegistration/src/main/java/com/bizen/listener/AssetCategoryListener.java
+++ hooks/BizenRegistration/src/main/java/com/bizen/listener/AssetCategoryListener.java
@@ -1,64 +1,39 @@
 package com.bizen.listener;
 
-import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.ModelListenerException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.BaseModelListener;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.GroupConstants;
-import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
-import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
-import com.liferay.portal.service.UserLocalServiceUtil;
-import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.asset.model.AssetCategory;
 
 public class AssetCategoryListener extends BaseModelListener<AssetCategory>{
 	
+	private static Log _log = LogFactoryUtil.getLog(AssetCategoryListener.class);
 	@Override
 	public void onAfterCreate(AssetCategory assetCategory)
 			throws ModelListenerException {
-		System.out.println("created asset category name : "+assetCategory.getName());
+		
+		_log.info("Inside onAfterCreate inside AssetCategoryListener class");
 		String siteName=assetCategory.getName();
-		String siteDescription=assetCategory.getName();
 		ServiceContext serviceContext = new ServiceContext();
 		String frindlyUrl="/";
-		System.out.println("created asset category name :3 "+assetCategory.getName());
-		try {
+		
+			try {
+				GroupLocalServiceUtil.addGroup(assetCategory.getUserId(), 0L, Group.class.getName(), ClassNameLocalServiceUtil.getClassNameId(Group.class.getName()), GroupConstants.DEFAULT_LIVE_GROUP_ID, assetCategory.getName(),
+						assetCategory.getName(), GroupConstants.TYPE_SITE_OPEN, true, GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION, frindlyUrl+siteName, true, true, serviceContext);
+			} catch (PortalException|SystemException e) {
+				e.getMessage();
+			} 
+			_log.info("site created by name "+siteName);
 			
-			Group  group= GroupLocalServiceUtil.createGroup(CounterLocalServiceUtil.increment());
-			long primaryId=CounterLocalServiceUtil.increment();
-			group.setActive(true);
-			group.setClassName(Group.class.getName());
-			group.setClassNameId(ClassNameLocalServiceUtil.getClassNameId(Group.class.getName()));
-			group.setClassPK(primaryId);
-			group.setCompanyId(PortalUtil.getDefaultCompanyId());
-			group.setDescription(siteDescription);
-			group.setCreatorUserId(assetCategory.getUserId());
-			group.setCreatorUserUuid(assetCategory.getUserUuid());
-			group.setFriendlyURL(frindlyUrl+siteName);
-			group.setGroupId(primaryId);
-			group.setLiveGroupId(GroupConstants.DEFAULT_LIVE_GROUP_ID);
-			group.setManualMembership(true);
-			group.setMembershipRestriction(GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION);
-			group.setName(siteName);
-			group.setNew(true);
-			group.setParentGroupId(0L);
-			group.setPrimaryKey(primaryId);
-			group.setRemoteStagingGroupCount(0);;
-			group.setSite(true);
-			group.setType(GroupConstants.TYPE_SITE_OPEN);
-			group.setUuid(serviceContext.getUuid());
-			group.setTreePath(group.buildTreePath());
-			GroupLocalServiceUtil.addGroup(group);
-			
-		  } catch (SystemException | PortalException  e) {
-			e.getMessage();
-		}
+		  
 		      
 		super.onAfterCreate(assetCategory);
 	}
